c5d10d384b9bed82e5f7d8b455c54a435bf8bf88
cdswyda <cdswyda@gmail.com>
2018-08-08 00:37:49 +0800

    add memory

---
+++ koa-session/app-koa-session-memory.js
@@ -0,0 +1,48 @@
+const Koa = require('koa')
+const session = require('koa-session')
+
+const app = new Koa()
+
+app.keys = ['this is koa session test']
+
+// memory base
+const sessions = {};
+app.use(session({
+    key: 'koa-session-test',
+    maxAge: 86400000,
+    overwrite: true,
+    /** (boolean) can overwrite or not (default true) */
+    httpOnly: true,
+    /** (boolean) httpOnly or not (default true) */
+    signed: true,
+    /** (boolean) signed or not (default true) */
+    rolling: false,
+    /** (boolean) Force a session identifier cookie to be set on every response. The expiration is reset to the original maxAge, resetting the expiration countdown. (default is false) */
+    renew: true,
+    /** (boolean) renew session when session is nearly expired, so we can always keep user logged in. (default is false)*/
+    store: {
+        async get(key) {
+            console.log(sessions, key)
+            return sessions[key]
+        },
+
+        async set(key, value) {
+            console.log(sessions)
+            sessions[key] = value
+        },
+        async destroy(key) {
+            sessions[key] = undefined
+        }
+    }
+}, app))
+
+app.use(ctx => {
+    // ignore favicon
+    if (ctx.path === '/favicon.ico') return;
+
+    let n = ctx.session.views || 0;
+    ctx.session.views = ++n;
+    ctx.body = n + ' views';
+})
+
+app.listen(3000)
